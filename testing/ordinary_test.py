5c892434a3852bc53cfbbb6df83ac3900f2121d0
VaderGong <gwd0451@163.com>
2023-11-03 11:42:44 +0800

    tree2

--- testing/ordinary_test.py
+++ testing/ordinary_test.py
@@ -1,9 +1,12 @@
 import gymnasium as gym
 import gym_testenvs
-import testing.data_process as data_process
+import data_process
 
 from stable_baselines3 import DQN,PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 n=1000
 
@@ -18,4 +21,21 @@ mean,rhw,var=data_process.calculate(episode_rewards)
 print(mean)
 print(rhw)
 print(var)
+
+def plot_mean(ys,xlabel,ylabel,legend,xlim,save_path=None):
+    plt.plot(ys)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(legend)
+    if save_path is not None:
+        plt.savefig(save_path)
+    plt.show()
+
+def plot_rhw(relative_half_width):
+    plt.plot(relative_half_width)
+    plt.xlabel('Number of Episodes')
+    plt.ylabel('Relative Half Width')
+    plt.legend(['Ordinary method'])
+    plt.show()
     
+plot_mean(mean)
