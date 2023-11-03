ddb665d001d4dca108eef43c7cfe36a6bce57357
VaderGong <gwd0451@163.com>
2023-11-03 12:54:14 +0800

    init

--- ordinary_test.py
+++ ordinary_test.py
@@ -14,14 +14,6 @@ env = gym.make('LunarLander/ordinary-v0',enable_wind=True,wind_power = 10.0,turb
 
 model = DQN.load("Rocket_agent_withwind/model/dqn_lunar.pkl")
 
-episode_rewards, episode_lengths = evaluate_policy(model, env, n_eval_episodes=n,return_episode_rewards = True,render=False)
-
-mean,rhw,var=data_process.calculate(episode_rewards)
-
-print(mean)
-print(rhw)
-print(var)
-
 def plot_mean(ys,xlabel,ylabel,legend,xlim,save_path=None):
     plt.plot(ys)
     plt.xlabel(xlabel)
@@ -32,13 +24,37 @@ def plot_mean(ys,xlabel,ylabel,legend,xlim,save_path=None):
         plt.savefig(save_path)
     plt.show()
 
-def plot_rhw(relative_half_width):
-    plt.plot(relative_half_width)
-    plt.xlabel('Number of Episodes')
-    plt.ylabel('Relative Half Width')
-    plt.legend(['Ordinary method'])
-    plt.show()
+# def reward():
+#     episode_rewards, episode_lengths = evaluate_policy(model, env, n_eval_episodes=n,return_episode_rewards = True,render=False)
+
+#     mean,rhw,var=data_process.calculate(episode_rewards)
+        
+#     plot_mean(mean,'Number of Episodes','Reward',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_mean.png')
+#     plot_mean(rhw,'Number of Episodes','Relative Half Width',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_rhw.png')
+#     plot_mean(var,'Number of Episodes','Variance',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_var.png')
+rewards = []
+crashes = []
+for i in range(n):
+    obs = env.reset()
+    done = False
+    reward = 0
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward_, tr, crash, info = env.step(action)
+        done= crash and tr
+        reward+=reward_
+        if done:
+            rewards.append(reward)
+            crashes.append(int(crash))
+
+mean_reward,rhw_reward,var_reward=data_process.calculate(rewards)
+mean_crash,rhw_crash,var_crash=data_process.calculate(crashes)
+
+plot_mean(mean_reward,'Number of Episodes','Reward',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_mean.png')
+plot_mean(rhw_reward,'Number of Episodes','Relative Half Width',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_rhw.png')
+
+plot_mean(mean_crash,'Number of Episodes','Crash Rate',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_mean_crash.png')
+plot_mean(rhw_crash,'Number of Episodes','Relative Half Width',['Ordinary method'],[0,1000],save_path='data/reward/ordinary_rhw_crash.png')
+
+
     
-plot_mean(mean,'Number of Episodes','Reward',['Ordinary method'],[0,1000],save_path='ordinary_mean.png')
-plot_mean(rhw,'Number of Episodes','Relative Half Width',['Ordinary method'],[0,1000],save_path='ordinary_rhw.png')
-plot_mean(var,'Number of Episodes','Variance',['Ordinary method'],[0,1000],save_path='ordinary_var.png')
